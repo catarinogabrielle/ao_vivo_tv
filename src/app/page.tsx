46dfffb036740b8d361a1ec3601ad7a5551702ae
Gabrielle <gabrielle.castro@ymonetize.com>
2024-03-28 16:32:56 -0300

    atualização tela home, componente card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
-import Image from 'next/image'
 import styles from "./page.module.scss";
+
+import Card from '../components/Card/page';
 
 export default function Home() {
   return (
@@ -10,21 +11,12 @@ export default function Home() {
         <h1 className={styles.title}>Ofertas para você</h1>
 
         <div className={styles.contentCard}>
-          <div className={styles.card}>
-            <Image
-              className={styles.img}
-              src="/product.png"
-              width={100}
-              height={100}
-              alt="Picture of the author"
-            />
-            <h1>Lancer Instant Contour Firming Facial Treatment Duo</h1>
-
-            <div>
-              <h2>R$188,00</h2>
-              <p>R$198,00</p>
-            </div>
-          </div>
+          <Card
+            img='/product.png'
+            title='Calsa marrom feminina que vai até o tornozelo'
+            price1='188,00'
+            price2='198,00'
+          />
         </div>
       </div>
     </div>
