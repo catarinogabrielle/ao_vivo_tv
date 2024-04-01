6f763ffbad2cbb2a32f257c131113332ab108c83
Gabrielle <gabrielle.castro@ymonetize.com>
2024-04-01 17:14:46 -0300

    atualização car

--- src/app/Car/page.tsx
+++ src/app/Car/page.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import styles from "./page.module.scss";
 import Button from '../../components/Button/pages';
 
@@ -5,46 +6,52 @@ export default function Car() {
   return (
     <div className={styles.container}>
       <div className={styles.content}>
-        <h1>Carrinho</h1>
-        <div className={styles.line} />
-        <div className={styles.order}>
-          <div>
-            <div className={styles.product}>
-              <img src="/productexemple.png" />
+        <h1 className={styles.title}>Carrinho</h1>
+
+        <div className={styles.contentMain}>
+          <div className={styles.boxCard}>
+            <div className={styles.card}>
+              <Image
+                className={styles.img}
+                src="/product.png"
+                width={100}
+                height={100}
+                alt="Picture of the author"
+              />
               <div>
-                <h2>Nome do produto que estamos vendendo no video</h2>
-                <div>
-                  <button>Excluir</button>
-                  <button>Salvar</button>
-                </div>
+                <nav>
+                  <h1>Nome do produto que estamos vedendo no video</h1>
+                  <p>Excluir</p>
+                </nav>
+
+                <select>
+                  <option value="1">1 un.</option>
+                  <option value="2">2 un.</option>
+                  <option value="3">3 un.</option>
+                </select>
               </div>
             </div>
-          </div>
-          <div className={styles.resume}>
-            <h3>RESUMO DA COMPRA</h3>
-            <div className={styles.line} style={{
-              marginBottom: '20px',
-              marginTop: '10px'
-            }} />
-            <div className={styles.detail}>
-              <div style={{
-                marginRight: '200px'
-              }}>
-                <p>Produto</p>
-                <br />
-                <p>Frete</p>
-              </div>
-              <div>
-                <p>R$120,00</p>
-                <br />
-                <p>R$40,00</p>
-              </div>
-            </div>
-            <Button text='Continuar a Compra' radius={0} />
           </div>
 
+          <div className={styles.boxInfo}>
+            <h2>Resumo Da Compra</h2>
+
+            <div className={styles.line} />
+
+            <div className={styles.info}>
+              <p>Produto</p>
+              <p>R$120,00</p>
+            </div>
+
+            <div style={{ marginBottom: 20 }} className={styles.info}>
+              <p>Frete</p>
+              <p>R$50,00</p>
+            </div>
+
+            <Button text='Efetuar a compra' radius={0} />
+          </div>
         </div>
       </div>
-    </div>
+    </div >
   );
 }
