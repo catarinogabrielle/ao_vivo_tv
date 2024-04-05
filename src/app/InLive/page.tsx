7e3afac90bd180f05ccaea499196d6290bfba57d
Gabrielle <gabrielle.castro@ymonetize.com>
2024-04-05 16:41:36 -0300

    atualização video

--- src/app/InLive/page.tsx
+++ src/app/InLive/page.tsx
@@ -7,48 +7,49 @@ export default function InLive() {
   return (
     <div className={styles.container}>
       <div className={styles.content}>
+        <h1 className={styles.title}>Canais:</h1>
+
         <div className={styles.channels}>
-          <h1>Canais:</h1>
-          <div className={styles.channel}>
+          <div>
             <p>Canal 1</p>
+          </div>
+
+          <div>
             <p>Canal 2</p>
+          </div>
+
+          <div>
             <p>Canal 3</p>
           </div>
         </div>
-        <div className={styles.banner}>
-          <div />
-          <div className={styles.boxInfo}>
-            <h1>Nome do produto que estamos vendendo no video</h1>
 
-            <div className={styles.line} />
-
-            <div className={styles.info}>
-              <h2>PREÇO DA VENDA</h2>
-            </div>
-
-            <div className={styles.info}>
-              <p>de R$198,00</p>
-              <h3>R$ 87,56</h3>
-            </div>
-
-            <Button text='Compre agora' radius={0} />
-          </div>
+        <div className={styles.boxVideo}>
+          <video className={styles.video} autoPlay controls preload="none" playsInline muted loop>
+            <source src="https://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4" type="video/mp4" />
+            <track
+              src="https://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4"
+              kind="subtitles"
+              srcLang="en"
+              label="English"
+            />
+            Your browser does not support the video tag.
+          </video>
         </div>
 
-        <div className={styles.recents}>
-          <h4>Itens recentementes no ar</h4>
-          <div className={styles.contentCard}>
-            <Link href='/Product'>
-              <Card
-                img='/product.png'
-                title='Calsa marrom feminina que vai até o tornozelo'
-                price1='188,00'
-                price2='198,00'
-              />
-            </Link>
-          </div>
-        </div>
+        <div className={styles.line} />
 
+        <h1 className={styles.title2}>Ofertas para você</h1>
+
+        <div className={styles.contentCard}>
+          <Link href='/Product'>
+            <Card
+              img='/product.png'
+              title='Calsa marrom feminina que vai até o tornozelo'
+              price1='188,00'
+              price2='198,00'
+            />
+          </Link>
+        </div>
       </div>
     </div>
   );
